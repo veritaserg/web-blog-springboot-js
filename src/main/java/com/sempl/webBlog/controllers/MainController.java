1298cc85be7b9c1e1d4b14550b8ccbb614f6d52a
veritaserg <infophotomasterscomua@gmail.com>
2020-04-06 21:15:35 +0300

    updated

--- src/main/java/com/sempl/webBlog/controllers/MainController.java
+++ src/main/java/com/sempl/webBlog/controllers/MainController.java
@@ -1,21 +1,37 @@
 package com.sempl.webBlog.controllers;
 
+import com.sempl.webBlog.models.Post;
+import com.sempl.webBlog.repo.PostRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
+    @Autowired
+   private PostRepository postService;
+
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("name", "main pages");
-        return "home";
+    public String main(Model model) {
+        return "main";
     }
 
     @GetMapping("/contact")
     public String contact() {
         return "contact";
     }
+
+    @GetMapping("/support")
+    public String support(Model model) {
+        Iterable<Post> posts = postService.findAll();
+
+        model.addAttribute("posts",posts);
+
+        return "support";
+    }
+
+
 }
