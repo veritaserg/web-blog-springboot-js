1fb5a2466e545638608219080e5e6456a20ae28d
veritaserg <infophotomasterscomua@gmail.com>
2020-04-12 11:38:26 +0300

    updated

--- src/main/java/com/sempl/webBlog/controllers/MainController.java
+++ src/main/java/com/sempl/webBlog/controllers/MainController.java
@@ -1,18 +1,23 @@
 package com.sempl.webBlog.controllers;
 
 import com.sempl.webBlog.models.Post;
-import com.sempl.webBlog.repo.PostRepository;
 
+import com.sempl.webBlog.service.PostService;
+import net.bytebuddy.description.type.TypeDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
 @Controller
 public class MainController {
 
     @Autowired
-   private PostRepository postService;
+    private PostService postService;
 
     @GetMapping("/")
     public String main(Model model) {
@@ -24,14 +29,16 @@ public class MainController {
         return "contact";
     }
 
-    @GetMapping("/support")
+    @GetMapping("/blog-list")
     public String support(Model model) {
         Iterable<Post> posts = postService.findAll();
-
-        model.addAttribute("posts",posts);
-
-        return "support";
+        ArrayList<Post> postList = new ArrayList<>();
+        Iterator iterator = posts.iterator();
+        while (iterator.hasNext()) {
+            postList.add((Post) iterator.next());
+        }
+        Collections.reverse(postList);
+        model.addAttribute("posts", postList);
+        return "blog-list";
     }
-
-
 }
