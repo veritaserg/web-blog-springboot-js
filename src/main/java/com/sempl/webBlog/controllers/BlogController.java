92ba192d7a553454769a1857719b85fa1cba4fea
veritaserg <infophotomasterscomua@gmail.com>
2020-04-20 11:35:13 +0300

    updated

--- src/main/java/com/sempl/webBlog/controllers/BlogController.java
+++ src/main/java/com/sempl/webBlog/controllers/BlogController.java
@@ -2,7 +2,7 @@ package com.sempl.webBlog.controllers;
 
 
 import com.sempl.webBlog.models.Post;
-import com.sempl.webBlog.repo.PostRepository;
+import com.sempl.webBlog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +15,7 @@ import java.util.Optional;
 public class BlogController {
 
     @Autowired
-    private PostRepository postRepository;
+    private PostService postService;
 
     @GetMapping("/blog/add")
     public String blogAdd(Model model) {
@@ -33,21 +33,57 @@ public class BlogController {
                 .full_text(full_text)
                 .build();
 
-        postRepository.save(post);
-
-        return "redirect:/support";
+        postService.save(post);
+        return "redirect:/blog-list";
     }
 
     @GetMapping("/blog/{id}")
     public String blogDetails(@PathVariable("id") Long id, Model model) {
-        if (!postRepository.existsById(id)){
-            return "redirect:/support";
+        if (!postService.existsById(id)) {
+            return "redirect:/blog-list";
         }
-        Optional<Post> post = postRepository.findById(id);
+        Optional<Post> post = postService.findById(id);
         ArrayList<Post> res = new ArrayList<>();
         post.ifPresent(res::add);
         model.addAttribute("post", res);
         return "blog-details";
     }
 
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable("id") Long id, Model model) {
+        if (!postService.existsById(id)) {
+            return "redirect:/support";
+        }
+        Optional<Post> post = postService.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostEdit(@PathVariable("id") Long id,
+                               @RequestParam String title,
+                               @RequestParam String anons,
+                               @RequestParam String full_text,
+                               Model model) {
+        Post post = Post.builder()
+                .id(id)
+                .title(title)
+                .anons(anons)
+                .full_text(full_text)
+                .build();
+
+        postService.save(post);
+        return "redirect:/blog-list";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostRemove(@PathVariable("id") Long id, Model model) {
+        if (!postService.existsById(id)) {
+            return "redirect:/support";
+        }
+        postService.deleteById(id);
+        return "redirect:/blog-list";
+    }
 }
