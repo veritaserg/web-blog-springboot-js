0617c1a51d60d598555b50295739675cf12c6b1f
veritaserg <infophotomasterscomua@gmail.com>
2020-04-21 14:00:40 +0300

    updated

--- src/main/java/com/sempl/webBlog/config/WebSecurityConfig.java
+++ src/main/java/com/sempl/webBlog/config/WebSecurityConfig.java
@@ -1,18 +1,23 @@
 package com.sempl.webBlog.config;
 
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .antMatcher("/**").authorizeRequests()
+                .antMatchers(new String[]{"/", "/not-restricted"}).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
     }
+
+
 }
